a4bf6b5be146fcb7be9e0a90a465eea338eda2eb
asma shahzadi <106690195+asma786shahzadi@users.noreply.github.com>
2024-08-12 10:20:59 -0700

    Create atm.py

---
+++ atm.py
@@ -0,0 +1,24 @@
+import time
+print("Please insert Your CARD")
+time.sleep(5)
+password=12345
+pin=int(input("Enter your ATM Pin "))
+balance=5000
+if(pin==password):
+    print("""
+           1==balance
+           2==withdraw balance
+           3==deposit balance
+           4==exit
+           """
+           )
+    try:
+        option=int(input("Please Enter Your Choice: "))
+    except:
+        print("Please Enter Valid Choice: ")
+
+    if(option==1):
+        print("Your Current Balance is {balance}")
+    if(option==2):
+        withdraw_amount=int(input("please enter withdraw_amount: "))
+        
