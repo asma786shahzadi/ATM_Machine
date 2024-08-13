bb5585c4fbfc6a1fa687abb055d4d4f206e8f468
asma shahzadi <106690195+asma786shahzadi@users.noreply.github.com>
2024-08-13 10:43:41 -0700

    Update atm.py

--- atm.py
+++ atm.py
@@ -5,21 +5,32 @@ password=12345
 pin=int(input("Enter your ATM Pin "))
 balance=5000
 if(pin==password):
-    print("""
-           1==balance
-           2==withdraw balance
-           3==deposit balance
-           4==exit
-           """
-           )
-    try:
-        option=int(input("Please Enter Your Choice: "))
-    except:
-        print("Please Enter Valid Choice: ")
+    while True:
+        print("""
+            1==balance
+            2==withdraw balance
+            3==deposit balance
+            4==exit
+            """
+            )
+        try:
+            option=int(input("Please Enter Your Choice: "))
+        except:
+            print("Please Enter Valid Choice: ")
 
-    if(option==1):
-        print("Your Current Balance is {balance}")
-    if(option==2):
-        withdraw_amount=int(input("please enter withdraw_amount: "))
-        balance=balance=withdraw_amount
-        
+        if(option==1):
+            print(f"Your Current Balance is {balance}")
+        if(option==2):
+            withdraw_amount=int(input("please enter withdraw_amount: "))
+            balance=balance-withdraw_amount
+            print(f"{withdraw_amount} is debited from your account")
+            print(f"Your updated current balance is {balance}")
+        if(option==3):
+            deposit_amount=int(input("Enter your Deposit Amount: "))
+            balance=balance+deposit_amount
+            print(f"{deposit_amount} is credited to your account")
+            print(f"Your updated current balance is {balance}")
+        if(option==4):
+            break
+else:
+    print("Wrong Pin! Please Enter again.....")
